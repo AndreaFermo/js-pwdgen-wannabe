8654c18d6ac5c0cc2f7ee24cd165c503fcc50a1b
Andrea Fermo <andreafermo.sd@gmail.com>
2023-02-21 14:07:07 +0100

    esercitazione conclusa

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const yourName = prompt('Digita il tuo nome');
+
+const yourLastName = prompt('Digita il tuo cognome');
+
+const favouriteColor = prompt('Digita il tuo colore prefeito');
+
+const currentYear = 23;
+
+document.getElementById('passwordGenerated').innerHTML = yourName + yourLastName + favouriteColor + currentYear;
